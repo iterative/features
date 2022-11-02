012c23fdae4c64d29dddcadc1a3cfbbab66ad747
Ivan Shcheklein <shcheklein@gmail.com>
2022-11-02 11:13:41 -0700

    dvc: add symlink to custom Codespaces git config
    Temporary solution to resolve this issue: https://github.com/community/community/discussions/38070

--- src/dvc/install.sh
+++ src/dvc/install.sh
@@ -12,3 +12,9 @@ apt-get install --yes dvc
 apt-get clean
 rm --recursive --force /var/lib/apt/lists/*
 
+# Temporary solution to resolve this issue:
+# https://github.com/community/community/discussions/38070
+GIT_CONFIG_PATH=/usr/local/etc/gitconfig
+if [ "${CODESPACES:-false}" = "true" -a -f $GIT_CONFIG_PATH ]; then
+  ln -s $GIT_CONFIG_PATH /etc/gitconfig || true
+fi

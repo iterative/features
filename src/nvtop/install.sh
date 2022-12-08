ddacf368f2fd1448dc2e05e5b55bd2f8c877eae6
Ivan Shcheklein <shcheklein@gmail.com>
2022-12-08 15:57:08 -0800

    install g++ via build essentials

--- src/nvtop/install.sh
+++ src/nvtop/install.sh
@@ -8,6 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update --yes
 apt-get install --yes libdrm-dev libsystemd-dev
 apt-get install --yes cmake libncurses5-dev libncursesw5-dev git
+apt-get install --yes build-essential
 git clone https://github.com/Syllo/nvtop.git
 mkdir -p nvtop/build && cd nvtop/build
 cmake .. -DNVIDIA_SUPPORT=ON -DAMDGPU_SUPPORT=ON -DINTEL_SUPPORT=ON
